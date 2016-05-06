d162ec0b424189c689695933721898b3803b7985
Titian Cernicova-Dragomir <dragomirtitian@yahoo.com>
2016-05-06 09:15:04 +0300

    Added nuget package info and remived autmatic package build from buid.

--- Dionysos.Db.Autofilter/Properties/AssemblyInfo.cs
+++ Dionysos.Db.Autofilter/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Dionysos.Db.Autofilter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Library that allows adding conditions automatically to queries and navigation properties.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Titian Cernicova-Dragomir")]
 [assembly: AssemblyProduct("Dionysos.Db.Autofilter")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
